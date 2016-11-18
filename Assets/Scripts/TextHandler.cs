90b7ad5cd820154bdc404c71740e666c5ab8b41e
Calichin53 <calichin53@gmail.com>
2016-11-18 09:31:38 -0500

    Script Texto

---
+++ Assets/Scripts/TextHandler.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextHandler : MonoBehaviour {
+
+	public string Texto;
+	string text;
+	int max,i;
+	public float Delay;
+	public Text Display;
+	float currentTime;
+	// Use this for initialization
+	void Start () {
+		text = "";
+		max = Texto.Length;
+		i = 0;
+		currentTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentTime += Time.deltaTime;
+		if (currentTime >= Delay) {
+			if (i <= max) {
+				text = Texto.Substring (0, i);
+				i++;
+				Display.text = text;
+				//Debug.Log (text);
+
+			}
+			currentTime = 0;
+		}
+	}
+}
