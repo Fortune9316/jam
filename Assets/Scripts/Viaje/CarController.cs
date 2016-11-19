051597d9b5160d9b07fb260794d1153fd9fd591e
carlos montaño rivera <carlos montao rivera>
2016-11-19 05:13:20 -0500

    escena viaje y oficina

---
+++ Assets/Scripts/Viaje/CarController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour {
+
+    float runningTime = 0f;
+    public float speed;
+    public GameObject llanta1,llanta2;
+    float cont;
+    bool flag;
+	// Use this for initialization
+	void Start () {
+        flag = true;
+        cont = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        OnCar();
+        if (flag)
+        {
+            GoOn();
+            rotateLL(llanta1, llanta2);
+        }
+        
+	}
+    void OnCar()
+    {
+        Vector3 newPos = transform.position;
+        float deltaHeight = (Mathf.Sin(runningTime + Time.deltaTime) - Mathf.Sin(runningTime));
+        newPos.y += deltaHeight * 0.08f;
+        runningTime += Time.deltaTime * 15f;
+        transform.position = newPos;
+    }
+    public void GoOn()
+    {
+        transform.position += new Vector3(5f * Time.deltaTime, 0f, 0f);
+        llanta1.transform.position += new Vector3(5f * Time.deltaTime, 0f, 0f);
+        llanta2.transform.position += new Vector3(5f * Time.deltaTime, 0f, 0f);
+    }
+    void rotateLL(GameObject a, GameObject b)
+    {
+        a.transform.Rotate(new Vector3(0f, 0f, 5f * Time.deltaTime + speed));
+        b.transform.Rotate(new Vector3(0f, 0f, 5f * Time.deltaTime + speed));
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        flag = false;
+    }
+
+    public void evasion()
+    {
+        transform.position += new Vector3(0f, -2f, 0f);
+        llanta1.transform.position += new Vector3(0f, -2f, 0f);
+        llanta2.transform.position += new Vector3(0f, -2f, 0f);
+    }
+
+}
---
+++ Assets/Scripts/Viaje/ViajeA.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViajeA : MonoBehaviour
+{
+    ColorController[] controladores;
+    public GameObject abuela;
+    public GameObject abuelaB;
+    public GameObject targetAbuela;
+    public GameObject secondTargetAbuela;
+    public GameObject car;
+    public GameObject carB;
+    private Vector3 posAbuela;
+    RaycastHit2D hit;
+    float cont;
+    float maxDistanceDelta;
+    bool flag;
+    bool flag2;
+    // Use this for initialization
+    void Start()
+    {
+        controladores = GetComponents<ColorController>();
+        flag = true;
+        flag2 = false;
+        cont = 0f;
+        posAbuela = abuela.transform.position;
+        hit = new RaycastHit2D();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(flag)
+        {
+            float step = 0.5f * Time.deltaTime;
+            abuela.transform.position = Vector3.MoveTowards(abuela.transform.position, targetAbuela.transform.position, step);
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero); ;
+        }
+        
+        abuelaB.transform.position = abuela.transform.position;
+        if (Vector3.Distance(abuela.transform.position, targetAbuela.transform.position) <= 0.2f)
+        {
+            print("entre");
+            cont += Time.deltaTime;
+            flag = false;
+        }
+            if(cont>=10f && flag2!=true)
+            {
+                float step1 = 0.5f * Time.deltaTime;
+                abuela.transform.position = Vector3.MoveTowards(abuela.transform.position, secondTargetAbuela.transform.position, step1);
+            }
+        if(hit.collider.gameObject.tag == "minusTime" && !flag2)
+        {
+            car.GetComponent<CarController>().evasion();
+            carB.GetComponent<CarController>().evasion();
+            flag2 = true;
+        }
+
+        if(flag2)
+        {
+            car.GetComponent<CarController>().GoOn();
+            carB.GetComponent<CarController>().GoOn();
+            for (int i = 0; i < controladores.Length; i++)
+            {
+                controladores[i].ToColor("Oficina_A");
+            }
+        }
+        
+    }
+}
